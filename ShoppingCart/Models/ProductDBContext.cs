fd1bfe0d7ba1184638561c228d1d6473a7cf8ec6
NguyenHoangHa9 <112064259+NguyenHoangHa9@users.noreply.github.com>
2023-10-24 01:18:09 +0700

    Shoping Cart

---
+++ ShoppingCart/Models/ProductDBContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ShoppingCart.Models
+{
+    public class ProductDBContext : DbContext
+    {
+        public ProductDBContext()
+        {
+        }
+
+        public ProductDBContext(DbContextOptions<ProductDBContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=LAPTOP-L55F4844\\SQLEXPRESS01;Initial Catalog=ShopingCart;User ID=sa;Password=123456;Encrypt=False");
+        }
+
+        public DbSet<Product> Products { get; set; }
+        
+        
+    }
+}
